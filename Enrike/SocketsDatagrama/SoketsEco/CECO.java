bd401c43452142222e9d4098deb3fd41d5573a38
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2019-02-13 21:16:51 -0600

    eco en desarrollo (dudas)

--- Enrike/SocketsDatagrama/SoketsEco/CECO.java
+++ Enrike/SocketsDatagrama/SoketsEco/CECO.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class CECO{
 	public static void main(String[] args){
 		try{
-			int limite = 65535;
+			int limite = 2000;
 			DatagramSocket cl = new DatagramSocket();
 			String msj = "Un mensaje sobre datagrams..";
 			byte[] b = msj.getBytes();
@@ -12,29 +12,32 @@ public class CECO{
 			int pto = 1234;
 			InetAddress dst = null;
 
-			if(b.length > limite) {
-
-				try {
-					dst = InetAddress.getByName(host);
-					ByteArrayInputStream bai2 = new ByteArrayInputStream(b);
-					byte[] b2 = new byte[65535];
-					int n = 0;
-					while((n = bai2.read(b2))! = -1) {
-						DatagramPacket p1 = new DatagramPacket(b2, n, dts, pto);
-						cl.send();
-					}
-				}
-				catch(UnknownHostException e){
-					System.err.println("La direccion no es valida.");
-					System.exit(1);
-				}//catch
+			try {
+				dst = InetAddress.getByName(host);
 			}
+			catch(UnknownHostException e){
+				System.err.println("La direccion no es valida.");
+				System.exit(1);
+			}//catch
 
-
-
+			if(b.length > limite) {
+				ByteArrayInputStream bais2 = new ByteArrayInputStream(b);
+				byte[] b2 = new byte[100];
+				int n = 0;
+				while((n = bais2.read(b2))! = -1) {
+					DatagramPacket p1 = new DatagramPacket(b2, n, dts, pto);
+					cl.send(p1);
+					DatagramPacket p2 = new DatagramPacket(new byte[100], 100);
+					cl.receive(p2);
+					System.out.println("Datagrama de eco recibido desde: " + p2.getAddress() + ": " + p2.getPort() + " con los datos:");
+					String datos = new String(p2.getData(), 0, p2.getLength());
+					System.out.println("Eco: " + datos);
+				}
+			}
 			DatagramPacket p = new DatagramPacket(b, b.length, dst, pto);
 			cl.send(p);
 			cl.close();
+
 		}catch(Exception e){
 			e.printStackTrace();
 		}//catch

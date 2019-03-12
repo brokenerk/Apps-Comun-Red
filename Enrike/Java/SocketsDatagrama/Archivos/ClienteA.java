8801ff7b63e644f7607ffbd844a195edb7360224
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2019-03-11 20:37:13 -0600

    envia archivos UDP

--- Enrike/Java/SocketsDatagrama/Archivos/ClienteA.java
+++ Enrike/Java/SocketsDatagrama/Archivos/ClienteA.java
@@ -2,20 +2,22 @@ import javax.swing.JFileChooser;
 import java.net.*;
 import java.io.*;
 import java.util.*;
+import java.lang.Thread;
 
 public class ClienteA{
 	public static void main(String[] args){
 		try{
 			JFileChooser jf = new JFileChooser();
-			//jf.setMultiSelectionEnbled(true);
+			jf.setMultiSelectionEnabled(true);
 			int r = jf.showOpenDialog(null);
 
 			if(r == JFileChooser.APPROVE_OPTION)
 			{
+				File[] files = jf.getSelectedFiles();
 				String host = "127.0.0.1";
 				int pto = 1234;
 				InetAddress dst = null;
-				int limite = 1040;
+				int limite = 2000;
 
 				try{
 					dst = InetAddress.getByName(host);
@@ -24,59 +26,61 @@ public class ClienteA{
 					System.exit(1);
 				}//catch
 
-				File f = jf.getSelectedFile();
-				String nombre = f.getName();
-				int tam = (int) f.length();
-				String path = f.getAbsolutePath();
-				int numPartes = tam / limite;
-				if(tam % limite > 0) 
-					numPartes++;
+				for(File f : files)
+				{
+					String nombre = f.getName();
+					int tam = (int) f.length();
+					String path = f.getAbsolutePath();
+					int numPartes = tam / limite;
+					if(tam % limite > 0) 
+						numPartes++;
 
-				ArrayList<byte[]> bytesArchivo = new ArrayList<byte[]>();
-				int parte = 0;
-				int n = 0;
-
-				DataInputStream dis = new DataInputStream(new FileInputStream(path));
-				
-				while(parte < numPartes){
-					byte[] b = new byte[limite];
-					n = dis.read(b);
-					bytesArchivo.add(parte, b);
-					parte++;
-				}//while
-				dis.close();
-
-				System.out.println("\nSe envia el archivo " + path + " con " + tam + " bytes de " + numPartes + " partes.");
-				
-				DatagramSocket cl = new DatagramSocket();
-				
-				String hs = nombre + "-" + tam + "-" + numPartes;
-				byte[] bhs = hs.getBytes();
-				DatagramPacket handshake = new DatagramPacket(bhs, bhs.length, dst, pto);
-				cl.send(handshake);
-				System.out.println("\nHandShake enviado al servidor.");
-
-				DatagramPacket respuesta = new DatagramPacket(new byte[65535], 65535);
-				for( ; ; ){	
-					cl.receive(respuesta);
-					String res = new String(respuesta.getData(), 0 , respuesta.getLength());
+					int parte = 1;
+					int n = 0;
+					System.out.println("\n\n-----------------------------------------------------------------------------");
+					System.out.println("Se envia el archivo " + path + " con " + tam + " bytes de " + numPartes + " partes.");
+					DatagramSocket cl = new DatagramSocket();
 					
-					if(res.equalsIgnoreCase("finalizado")){
-						System.out.println("Archivo enviado.");
-						break;
-					}//if
-					else{
-						int parteReq = Integer.parseInt(res);
-						byte[] enviar = bytesArchivo.get(parteReq);
-						System.out.println("Enviando parte: " + parteReq);
-						
-						DatagramPacket enviarParte = new DatagramPacket(enviar, enviar.length, dst, pto);
-						cl.send(enviarParte);
-						System.out.println("Bytes enviados.");
-					}//else
-				}//for
+					/*Handshake*/
+					String hs = nombre + "/" + tam + "/" + numPartes;
+					byte[] bhs = hs.getBytes();
+					DatagramPacket handshake = new DatagramPacket(bhs, bhs.length, dst, pto);
+					cl.send(handshake);
+					System.out.println("HandShake enviado al servidor con datos del archivo.\n");
 
-				cl.close();
+					DataInputStream dis = new DataInputStream(new FileInputStream(path));
+					DatagramPacket respuesta = new DatagramPacket(new byte[65535], 65535);
+					int bytesEnviados = 0;
+
+					for( ; ; ){	
+						cl.receive(respuesta);
+						System.out.println("\nDatagrama recibido desde: " + respuesta.getAddress() + ": " + respuesta.getPort());
+						String res = new String(respuesta.getData(), 0 , respuesta.getLength());
+						
+						if(res.equalsIgnoreCase("finalizado")){
+							System.out.println("El servidor indica que se recibieron todas las partes. Archivo " + nombre + " enviado de " + bytesEnviados + " bytes.");
+							break;
+						}//if
+						else{
+							int parteReq = Integer.parseInt(res);
+							
+							if(parte == parteReq){
+								byte[] b = new byte[limite];
+								n = dis.read(b);
+								System.out.print("Enviando parte: " + parteReq);
+								DatagramPacket enviarParte = new DatagramPacket(b, n, dst, pto);
+								cl.send(enviarParte);
+								System.out.println(". " + n + " bytes enviados.");
+								parte++;
+								bytesEnviados = bytesEnviados + n;
+								Thread.sleep(50);
+							}
+						}//else
+					}//for
+
+					dis.close();
+					cl.close();
+				}//for
 			}//if
 		}catch(Exception e){
 			e.printStackTrace();
--- Enrike/Java/SocketsDatagrama/Archivos/ServidorA.java
+++ Enrike/Java/SocketsDatagrama/Archivos/ServidorA.java
@@ -1,6 +1,7 @@
 import java.net.*;
 import java.io.*;
 import java.util.*;
+import java.lang.Thread;
 
 public class ServidorA{
 	public static void main(String[] args){
@@ -13,21 +14,24 @@ public class ServidorA{
 				s.receive(p);
 				InetAddress dir = p.getAddress();
 				int pto = p.getPort();
+				System.out.println("\n\n-----------------------------------------------------------------------------");
 				System.out.println("Datagrama recibido desde: " + dir + ": " + pto);
 				
 				String hs = new String(p.getData(), 0, p.getLength());
 
-				String[] handshake = hs.split("-");
+				String[] handshake = hs.split("/");
 				String nombre = handshake[0];
 				int tam = Integer.parseInt(handshake[1]);
 				int numPartes = Integer.parseInt(handshake[2]);
-				System.out.println("HandShake recibido. Archivo: " + nombre + " de tam " + tam + " de " + numPartes + " partes.");
-				int cont = 0;
+				System.out.println("HandShake recibido. Archivo: " + nombre + " de tam " + tam + " de " + numPartes + " partes.\n");
+				int cont = 1;
 
-				ArrayList<byte[]> bytesArchivo = new ArrayList<byte[]>();
+				ArrayList<Boolean> banderas = new ArrayList<Boolean>(numPartes);
+				Collections.fill(banderas, false);
+				DataOutputStream dos = new DataOutputStream(new FileOutputStream(nombre));
+				int bytesRecibidos = 0;
 
-
-				while(cont < numPartes){
+				while(cont <= numPartes || banderas.contains(false)){
 					String parte = Integer.toString(cont);
 					byte[] bParte = parte.getBytes();
 					DatagramPacket parteReq = new DatagramPacket(bParte, bParte.length, dir, pto);
@@ -36,34 +40,35 @@ public class ServidorA{
 
 					DatagramPacket parteArchivo = new DatagramPacket(new byte[65535], 65535);
 					s.receive(parteArchivo);
-					bytesArchivo.add(cont, parteArchivo.getData());
-					System.out.println("Bytes recibidos.");
-					cont++;
+					System.out.println("Datagrama recibido desde: " + parteArchivo.getAddress() + ": " + parteArchivo.getPort());
+
+					if(parteArchivo.getData() != null){
+						byte[] b = new byte[2000];
+						int n = 0;
+						banderas.add(cont - 1, true);
+						DataInputStream dis = new DataInputStream(new ByteArrayInputStream(parteArchivo.getData()));
+						n = dis.read(b);
+						
+						if(tam < 2000)
+							n = tam;
+
+						dos.write(b, 0, n);
+						dos.flush();
+						dis.close();
+						Thread.sleep(50);
+						cont++;
+						tam = tam - n;
+						bytesRecibidos = bytesRecibidos + n;
+						System.out.println("Parte " + parte + " recibida de " + n + ". Escribiendo bytes en archivo temporal...\n");
+					}
 				}
+				dos.close();
 
 				String fin = "finalizado";
 				byte[] bfin = fin.getBytes();
 				DatagramPacket pfin = new DatagramPacket(bfin, bfin.length, dir, pto);
 				s.send(pfin);
-				System.out.println("Total de partes recibidas, construyendo archivo...");
-
-				DataOutputStream dos = new DataOutputStream(new FileOutputStream(nombre));
-				
-				int n = 0, i = 0;
-				byte[] b = new byte[1040];
-
-				while(i < numPartes){
-					DataInputStream dis = new DataInputStream(new ByteArrayInputStream(bytesArchivo.get(i)));
-                    while((n = dis.read(b)) != -1){
-                        dos.write(b, 0, n);
-                        dos.flush();
-                    }
-                    dis.close();
-                    i++;
-				}
-				dos.close();
-
-				System.out.println("Archivo " + nombre + " escrito.");
+				System.out.println("\nTotal de partes recibidas, construyendo archivo... Archivo " + nombre + " escrito de " + bytesRecibidos + " bytes.");
 			}//for
 		}catch(Exception e){
 			e.printStackTrace();

b039ffd98660f589b4cae7a0ecc49c76361d2bc1
Enrike Ramos <39656338+brokenerk@users.noreply.github.com>
2019-03-13 19:50:37 -0600

    bd acorde a las clases propuestas

--- 1erExamen/Publicacion.java
+++ 1erExamen/Publicacion.java
@@ -1,12 +1,18 @@
 public class Publicacion{
+	private int Id;
 	private Comentario[] comentarios;
 	private String nombre;
 	private String fecha;
 
-	public Publicacion(Comentario[] comentarios, String nombre, String fecha){
+	public Publicacion(int Id, Comentario[] comentarios, String nombre, String fecha){
+		this.Id = Id;
 		this.comentarios = Arrays.copyOf(comentarios, comentarios.length);
 		this.nombre = nombre;
 		this.fecha = fecha;
+	}
+
+	public int getId(){
+		return this.Id;
 	}
 
 	public Comentario[] getComentarios(){
